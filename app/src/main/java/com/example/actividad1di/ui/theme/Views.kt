2d0218f049e638925bcedc848cc75bc94dcfb1b7
Adrisab16 <adrsabper@gmail.com>
2024-01-31 21:14:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/actividad1di/ui/theme/Views.kt
@@ -0,0 +1,16 @@
+package com.example.actividad1di.ui.theme
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.actividad1di.cabezera.Cabezera
+import com.example.actividad1di.portada.Portada
+
+@Composable
+fun PortadaView(){
+    Cabezera(modifier = Modifier.size(0.000000000000000000000000000000001.dp))
+
+}
