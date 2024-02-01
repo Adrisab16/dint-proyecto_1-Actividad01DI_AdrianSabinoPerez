c8bc2dd4489c226fe034439fedb97e034ca9627a
Adrián <116183531+Adrisab16@users.noreply.github.com>
2024-02-01 23:14:09 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/actividad1di/MainActivity.kt
+++ app/src/main/java/com/example/actividad1di/MainActivity.kt
@@ -9,8 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.actividad1di.championcards.SETTCARD
-import com.example.actividad1di.infomorde.InfoMorde
 import com.example.actividad1di.ui.theme.Actividad1DITheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InfoTop()
+                    PortadaView()
                 }
             }
         }
@@ -37,3 +35,17 @@ fun AppPreview() {
         SeleccionRolView()
     }
 }
+
+/*
+Aclaración: Estoy escribiendo esto a las 23:03 del 01/02/2024. El IDE en mi 
+ordenador ha dejado de funcionar (se queda haciendo build de forma indefinida, 
+ya que llevo 30 minutos esperando que termine de realizar el build), por lo 
+que los últimos cambios los estoy haciendo a ciegas. Como el resto de compañeros 
+tienen portátil, no les ocurre este error, ya que trabajan con el mismo portatil 
+en su casa y en clase, no es mi caso, a mi me ocurren estos inconvenientes que me impiden 
+seguir mi trabajo.
+
+Quería hacer esta pequeña aclaración ya que los cambios a ciegas que estoy haciendo, 
+los subiré a Github, el zip subido a Moodle solo contiene los cambios realizados hasta 
+las 21:33 del 01/02/2024.
+*/
